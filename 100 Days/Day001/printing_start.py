306adaf498c311a6cb96d8cb8e2fbe19774f0706
Michelle K Siaw <104367062+MKSIAW@users.noreply.github.com>
2024-07-19 09:13:52 +0000

    new commit

--- 100 Days/Day001/printing_start.py
+++
@@ -1,4 +0,0 @@
-print("Welcome to the Band Name Genrator.")
-city = input("What's name of the city you grew up in? ")
-pet = input("What's your pet's name? ")
-print("Your band name could be " + city +  " " + pet)
